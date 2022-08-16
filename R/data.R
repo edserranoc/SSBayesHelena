a5a3296092e31e4c865f8d7e3ff3f4a777f17046
edserranoc <86541614+edserranoc@users.noreply.github.com>
2022-08-15 19:27:24 -0500

    Upload Genodata1 description

--- R/data.R
+++ R/data.R
@@ -1,15 +1,12 @@
-#' Name of Data
+#'Genodata1
 #'
-#'A data frame or matrix containing genotypic 
-#'data from m markers as well as a column indicating the subpopulation 
-#'to which the individual belongs. 
-#'Subpopulations must be coded using 
+#'A data frame or matrix containing genotypic
+#'data from 49 markers as well as a column indicating the subpopulation
+#'to which the individual belongs.
+#'Subpopulations must be coded using
 #'consecutive integer numbers starting from 1.
-#'
+#' @docType data
+#' @keywords data
+#' @author Carlos Alberto Martínez Niño \email{cmartinez@@agrosavia.co}
 #' @format A data frame with 1247 rows and 50 variables:
-#' \describe{
-#'
-#'   ...
-#' }
-#' @source \url{your_URL_}
 "Genodata1"
