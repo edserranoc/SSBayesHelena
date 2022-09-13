8dfbb7265d4d39607420a8caf6c66bce1039c1bb
edserranoc <86541614+edserranoc@users.noreply.github.com>
2022-09-12 23:18:02 -0500

    Update Datasets

--- R/PostSamp_Neutral.R
+++ R/PostSamp_Neutral.R
@@ -34,7 +34,7 @@
 #'@export
 #'
 #'@examples Data=Data1
-#'Ex1=PostSamp_Neutral(Data,Prior=c(1/2,1/3),N.Samples=5000,Pop.col=1,Geno.cols= c(2:ncol(Genodata1)))
+#'Ex1=PostSamp_Neutral(Data,Prior=c(1/2,1/3),N.Samples=5000,Pop.col=1,Geno.cols= c(2:ncol(Data)))
 #'summary(Ex1$Posterior_Means)
 
 PostSamp_Neutral=function(Data,Prior=c(1/2,1/2),N.Samples,Pop.col,Geno.cols){
--- data-raw/Genodata1.R
+++
@@ -1,6 +0,0 @@
-library(usethis,readr)
-
-Genodata1 <- read_csv("data-raw/Genodata1.csv",col_names = FALSE)
-
-usethis::use_data(Genodata1, overwrite = TRUE)
-
